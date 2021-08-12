2997869bf979aca880064c41268d41f9756754c3
BrandonPetersen <70966818+BrandonPetersen@users.noreply.github.com>
2021-08-11 22:59:49 -0700

    Update application.py

--- webcode/application.py
+++ webcode/application.py
@@ -38,7 +38,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///crypto.db")
+db = SQL("sqlite:////home/brandonpetersen/cryptoutlook/crypto.db")
 
     
 
